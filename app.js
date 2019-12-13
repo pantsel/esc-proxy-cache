5a052fd84849083a899a65cc2c6e873beeb752bd
pantsel <tselentispanagis@gmail.com>
2019-12-13 22:02:06 +0100

    Refactoring

--- app.js
+++ app.js
@@ -9,13 +9,6 @@ const init = async () => {
 
     await Cache.setStrategy('memory').init();
     await Server.lift();
-    console.log('Server running on %s', server.info.uri);
 };
-
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
-});
 
 init();
