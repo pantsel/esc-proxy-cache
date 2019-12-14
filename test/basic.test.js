3e4f7f6e5a9733dd81fb52135d5923b086942efc
pantsel <tselentispanagis@gmail.com>
2019-12-14 18:02:09 +0100

    Added basic test

---
+++ test/basic.test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Lab = require('@hapi/lab');
+const { expect } = require('@hapi/code');
+const { afterEach, beforeEach, describe, it } = exports.lab = Lab.script();
+const { init } = require('../lib/server');
+
+describe('Basic test', () => {
+    let server;
+
+    beforeEach(async () => {
+        server = await init();
+    });
+
+    afterEach(async () => {
+        await server.stop();
+    });
+
+    it('responds with 200', async () => {
+        const res = await server.inject({
+            method: 'get',
+            url: '/api/v1/info'
+        });
+        expect(res.statusCode).to.equal(200);
+        expect(res.payload).to.contains(['version', 'date']);
+    });
+});
