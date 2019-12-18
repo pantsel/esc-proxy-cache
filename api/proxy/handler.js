eacf698c8585d35dcc97de647dea1995cf0a2051
pantsel <tselentispanagis@gmail.com>
2019-12-18 20:50:37 +0100

    Ref

--- api/proxy/handler.js
+++ api/proxy/handler.js
@@ -6,7 +6,6 @@ const Wreck = require('@hapi/wreck');
 const Cache = require('../../lib/cache');
 const Events = require('../../lib/events');
 
-
 module.exports = {
     proxy: (request, h) => {
         return h.proxy(_.merge(Config.proxy, {
