cd714ee599b46b82149ac131d849a331c3e5335b
pantsel <tselentispanagis@gmail.com>
2019-12-21 22:18:48 +0100

    ref

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ if(process.env !== 'production') process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
 const init = async () => {
     await Cache.setStrategy(Config.cache.strategy).init();
-    await Events.setStrategy("memory").init();
+    await Events.setStrategy(Config.pubSub.strategy).init();
     await Server.lift();
 };
 
