11b0f78c374648777fb8e226eacba72201374444
pantsel <tselentispanagis@gmail.com>
2019-12-29 17:04:49 +0200

    ref

--- config/pubsub.js
+++ config/pubsub.js
@@ -1,4 +1,4 @@
 module.exports = {
     strategy: process.env.PUBSUB_STRATEGY || 'memory',
-    subscriptionTimeout: parseInt(process.env.SUBSCRIPTION_TIMEOUT) || 30000
+    subscriptionTimeout: parseInt(process.env.PUBSUB_SUBSCRIPTION_TIMEOUT) || 30000
 };
