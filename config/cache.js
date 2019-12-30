6524ecb2858354b199b220826f72d077cac55564
pantsel <tselentispanagis@gmail.com>
2019-12-30 18:59:24 +0200

    Added CACHE_ROLLING_TTL as env config

--- config/cache.js
+++ config/cache.js
@@ -1,5 +1,6 @@
 module.exports = {
     strategy: process.env.CACHE_STRATEGY || 'memory',
     endpoints: process.env.CACHE_ENDPOINTS_JSON_FILE_PATH || process.cwd() + '/test/upstream-server/cacheable-endpoints.json',
-    ttl: process.env.CACHE_TTL ? parseInt(process.env.CACHE_TTL) : 30000
+    ttl: process.env.CACHE_TTL ? parseInt(process.env.CACHE_TTL) : 30000,
+    rollingTTL: process.env.CACHE_ROLLING_TTL === 'true'
 };
